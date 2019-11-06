dea28f981209f6350b510d952291478924106442
Hailecho <56434119+Hailecho@users.noreply.github.com>
2019-11-06 16:47:34 +0100

    content added

--- file_example.py
+++ file_example.py
@@ -1,1 +1,19 @@
+x= int(input('Give your number of sides: '))
+r = int(input('Give your second number: '))
+m = len(str(x*r))
+
+for i in range(r+1):
+    t = len(str(x*i))
+    k = len(str(r))-len(str(i))
+    # print(x, ' times ',' '*k, i,' = ',' '*(m-t), x*i, sep='')
+
+# Another way of doing it is using sequence of print with end option:
+
+for i in range(r + 1):
+    t = len(str(x*i))
+    k = len(str(r))-len(str(i))
+    print(x, 'times', end='')
+    print(' '*k, i,'=', end='')
+    print(' '*(m-t), x*i)
+    # print(x, ' times ',' '*k, i,' = ',' '*(m-t), x*i)
 
